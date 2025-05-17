8a50ffd693dfa0107c215b94eb78f5c187ce0edf
rayanramoul <r.ramoul@instadeep.com>
2025-05-18 00:54:48 +0200

    feat: detect directory or file input path

--- src/anknote/io/gather.py
+++ src/anknote/io/gather.py
@@ -1,5 +1,11 @@
+from loguru import logger
 from pathlib import Path
 
 
 def retrieve_input(input_path: Path) -> dict:
-    pass
+    print(input_path.glob("*"))
+    if input_path.is_dir():
+        logger.debug("Input path is a directory")
+
+    else:
+        logger.debug("Input path is a file")
